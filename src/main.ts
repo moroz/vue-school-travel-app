d1e2a7ca9ad602cbbe433215ed8c224b9b9d9cd9
Karol Moroz <k.j.moroz@gmail.com>
2022-03-14 22:52:00 +0100

    Create About and Home pages, set up router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,27 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+import App from "./App.vue";
+import Home from "@/views/Home.vue";
+import About from "@/views/About.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: About
+    }
+  ]
+});
+
+app.use(router);
+
+app.mount("#app");
